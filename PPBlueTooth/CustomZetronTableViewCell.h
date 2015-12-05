5f50a95e7b46faf29c58c5c89aa675f0ca36abce
Jamaal Sedayao <you@example.com>
2015-12-05 18:56:51 -0500

    jamaal push - working on custom tableview cell added RSSI func

---
+++ PPBlueTooth/CustomZetronTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomZetronTableViewCell.h
+//  PPBlueTooth
+//
+//  Created by Jamaal Sedayao on 12/5/15.
+//  Copyright © 2015 apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomZetronTableViewCell : UITableViewCell
+
+@end
---
+++ PPBlueTooth/CustomZetronTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  CustomZetronTableViewCell.m
+//  PPBlueTooth
+//
+//  Created by Jamaal Sedayao on 12/5/15.
+//  Copyright © 2015 apps. All rights reserved.
+//
+
+#import "CustomZetronTableViewCell.h"
+
+@implementation CustomZetronTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
